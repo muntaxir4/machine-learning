73e8e7b6d00bd15af1808bbdcf4567b2afb4a5d7
muntaxir4 <mjmullick0608@gmail.com>
2025-09-14 19:45:31 +0530

    fit and plot linear regression; add datasets

---
+++ supervised_learning/regression.py
@@ -0,0 +1,46 @@
+## Creating features
+import numpy as np
+
+# Create X from the radio column's values
+X = sales_df["radio"].values
+
+# Create y from the sales column's values
+y = sales_df["sales"].values
+
+# Reshape X
+X = X.reshape(-1, 1)
+
+# Check the shape of the features and targets
+print(X.shape, y.shape)
+
+
+## Building a linear regression model
+# Import LinearRegression
+from sklearn.linear_model import LinearRegression
+
+# Create the model
+reg = LinearRegression()
+
+# Fit the model to the data
+reg.fit(X, y)
+
+# Make predictions
+predictions = reg.predict(X)
+
+print(predictions[:5])
+
+
+## Visualizing a linear regression model
+# Import matplotlib.pyplot
+import matplotlib.pyplot as plt
+
+# Create scatter plot
+plt.scatter(X, y, color="blue")
+
+# Create line plot
+plt.plot(X, predictions, color="red")
+plt.xlabel("Radio Expenditure ($)")
+plt.ylabel("Sales ($)")
+
+# Display the plot
+plt.show()
